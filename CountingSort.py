b9989e34552b9346e31d2f632763ebf96e3672d4
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2022-01-26 01:40:06 -0600

    Update CountingSort.py

--- CountingSort.py
+++ CountingSort.py
@@ -1,10 +1,5 @@
-from posixpath import split
-import re
-
-
 def countingSort(arr):
     data=[0 for i in range(100)]
     for i in range(len(arr)):
         data[arr[i]]+=1
     return data
-print(countingSort([1,1,3,2,1]))
