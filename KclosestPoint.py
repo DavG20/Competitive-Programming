db54eb4f103a5437c40b2dd28e44edf12372d21a
DavG20 <dawit201121@gmail.com>
2022-01-27 03:04:20 -0600

    add KclosestPoint

--- KclosestPoint.py
+++ KclosestPoint.py
@@ -1,7 +1,9 @@
 class Solution:
     def kClosest(self, points,k):
-        points.sort(key=self.squared_distance)
+        points.sort(key=self.distance)
         return points[:k]
     
-    def squared_distance(self, point):
+    def distance(self, point):
         return point[0] ** 2 + point[1] ** 2
+x=Solution()
+print(x.kClosest([[0, 2], [-3, 3], [-2, 5], [6, 10]],3))
