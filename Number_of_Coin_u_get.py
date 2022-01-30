f4f60f05355df5b2b15c01c45830c423864022fc
DavG20 <dawit201121@gmail.com>
2022-01-30 07:13:11 -0600

    coins get

--- Number_of_Coin_u_get.py
+++ Number_of_Coin_u_get.py
@@ -1,18 +1,18 @@
-from this import d
-
-
 class Solution(object):
     def maxCoins(self,piles):
         piles.sort()
         count=int(len(piles))
         coin=0
         while count>0:
-            data=piles[-3:]
-            coin+=data[1]
+            data=piles[-2:]
+            coin+=data[0]
             piles.pop()
             piles.pop()
-            piles.pop()
+            piles.remove(min(piles))
             print(data, data[1])
             print(count)
             count-=3
+        print(coin)
         return coin
+c=Solution()
+c.maxCoins([1,2,3,4,5,6,7,8,9])
