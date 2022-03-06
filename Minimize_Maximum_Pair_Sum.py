d2513f09a72800fe2e519b6009e860e2179dced2
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2022-03-06 11:29:55 +0300

    Update Minimize_Maximum_Pair_Sum.py

--- Minimize_Maximum_Pair_Sum.py
+++ Minimize_Maximum_Pair_Sum.py
@@ -8,5 +8,3 @@ class Solution:
         for i in range(int(len(nums)/2)):
             data+=[nums[i]+nums[len(nums)-i-1]]
         return max(data)    
-c=Solution()
-print(c.minPairSum([4,1,5,1,2,5,1,5,5,4]))
