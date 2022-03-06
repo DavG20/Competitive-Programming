6b739f2f3c550a823017146ec4c857775ce466db
DavG20 <dawit201121@gmail.com>
2022-03-07 00:27:02 +0300

    add max num

---
+++ MAx_numberof_K_sum.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+
+class Solution(object):
+    def maxOperations(self, nums, k):
+        hash = defaultdict(int)
+        count = 0 
+        for num in nums:
+            target = k - num
+            print(hash[target])
+            if hash[target] > 0:
+                hash[target] -= 1
+                count += 1
+            else:
+                hash[num] += 1
+                
+        return count 
+        
+c=Solution()
+print(c.maxOperations([1,2,3,4],
+5))
