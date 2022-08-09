af74da513bbf47f9eabad405a3926be4b388cc6f
DavG20 <dawit201121@gmail.com>
2022-08-09 17:13:01 +0300

    Stack

--- LeetCode/Queue.py
+++ LeetCode/Queue.py
@@ -1,20 +1,27 @@
-class Queue(object):
+class MyQueue(object):
+    
     def __init__(self):
-        self.Stack=[]
-    def push(self,x):
-        print(len(self.Stack),"this is len")
-        self.Stack.insert(len(self.Stack),x)
-    def Pop(self):
-        pop_return=self.Stack[0]
-        self.Stack.remove(self.Stack[0])
-        return pop_return
-    def display(self):
-        for i in self.Stack:
-            print(i)
+        self.stack=[]
         
-q=Queue()
-q.push(9)
-q.push(0)
-q.push(2)
-print(q.Pop())
-q.display()
+
+    def push(self, x):
+        self.stack.insert(len(self.stack),x)
+        
+
+    def pop(self):
+       
+        pop_value=self.stack[0]
+        self.stack.remove(self.stack[0])
+        return pop_value
+        
+
+    def peek(self):
+       
+        return self.stack[0]
+        
+
+    def empty(self):
+        return len(self.stack)==0
+        
+
+
