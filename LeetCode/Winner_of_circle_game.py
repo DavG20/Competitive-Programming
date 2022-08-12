12a7a73c93ee3b5e4df62ca559fc10bf207c099d
DavG20 <dawit201121@gmail.com>
2022-08-12 20:37:59 +0300

    Power of three

--- LeetCode/Winner_of_circle_game.py
+++ LeetCode/Winner_of_circle_game.py
@@ -1,9 +1,9 @@
-class Solution:
-    def findTheWinner(self, n, k):
-        ans = 0
-        for i in range(2, n + 1):
-            ans = (ans + k) % i
-        return ans + 1
-                
-c=Solution()
-c.findTheWinner(8, 7)
+class Solution(object):
+    def findTheWinner(self,n,k):
+        ans=0
+        for i in range(1,n):
+            ans=(ans+k)%i
+        return ans +1
+    
+            
+    
