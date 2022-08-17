1ee4ae3f73ccdd1addc79eef245c8a2c19635591
DavG20 <dawit201121@gmail.com>
2022-08-17 15:25:38 +0300

    next greater node

--- LeetCode/Insertion_Sort_List.py
+++ LeetCode/Insertion_Sort_List.py
@@ -3,17 +3,19 @@
 #         self.next=None 
 #         self.val=val 
 
-class Solution(object ):
+class Solution(object):
     def insertionSortList(self,head):
         newNode=ListNode(0)
         current_node=head 
+        if head==None:
+            return head 
         while current_node:
             prev_node=newNode
             while prev_node.next and prev_node.next.val<current_node.val:
-                prev_node=prev_node.next
+                prev_node=prev_node.next 
             next_node=current_node.next 
             current_node.next=prev_node.next 
             prev_node.next=current_node
             current_node=next_node
-        return newNode.next         
+        return newNode.next        
         
