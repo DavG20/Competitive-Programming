3416fe27426063a955e021a71efc737671ce86a2
DavG20 <dawit201121@gmail.com>
2022-08-21 23:01:01 +0300

    product of array

--- LeetCode/Subarray_Sum_Equals_K.py
+++ LeetCode/Subarray_Sum_Equals_K.py
@@ -1,8 +1,9 @@
+from collections import defaultdict
 class Solution(object):
     def subarraySum(self,nums,k):
-        dic =defaultdict(int)
-        sum=0
+        dic=defaultdict(int)
         result=0
+        sum=0
         dic[0]=1
         for num in nums:
             sum+=num
