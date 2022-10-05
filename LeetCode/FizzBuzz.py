59fa29f5e8ba66c3ce37f1ff5f802b0454882c64
DavG20 <dawit201121@gmail.com>
2022-10-05 08:50:39 +0300

    working on label

--- LeetCode/FizzBuzz.py
+++ LeetCode/FizzBuzz.py
@@ -3,7 +3,6 @@ class Solution(object):
     def fizzBuzz(self,n):
         output=[]
         for i in range(1,n+1):
-            print(i)
             if i%3==0 and i%5==0:
                 output+=["FizzBuzz"]
             elif i%5==0:
@@ -14,5 +13,4 @@ class Solution(object):
             else:
                 output+=[str(i)]
         return output
-c=Solution()
-print(c.fizzBuzz(15))
+
