8501346c7a18491bc5cb1afe6d2679522ef8c9ae
DavG20 <dawit201121@gmail.com>
2022-10-14 15:18:14 +0300

    rearrange array

---
+++ LeetCode/Not_equal_To_Its_Neighbor_avrage.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rearrangeArray(self,nums):
+        nums.sort()
+        left=0
+        right=len(nums)-1
+        result=[]
+
+        while left<=right:
+            if left!=right:
+                result.append(nums[right])
+                result.append(nums[left])
+            else:
+                result.append(nums[left])
+        return result
