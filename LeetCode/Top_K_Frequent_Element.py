f0b456581c4019e8b5391ff2f52007b42ad4a48c
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2022-10-15 19:42:40 +0300

    Update Top_K_Frequent_Element.py

--- LeetCode/Top_K_Frequent_Element.py
+++ LeetCode/Top_K_Frequent_Element.py
@@ -1,18 +1,11 @@
 
 class Solution(object):
     def topKFrequent(self,nums,k):
-        Freq_Dic={}
-        Output_Store=[]
-        
-        for value in nums:
-            if value  in Freq_Dic:
-                Freq_Dic[value]+=1
-            else:
-                Freq_Dic[value]=1
-        final_Dic=sorted(Freq_Dic.items(),key=lambda x: x[1], reverse=True)  
-        for i in range(k):
-            Output_Store.append(final_Dic[i][0])  
-        Output_Store.sort()
-        return(Output_Store)   
+        count=Counter(nums)
+        count=sorted(count.items(),kay= lambda x:x[1],reverse=True)
+        res=[]
+        for num in count:
+            res.append(num)
+        return res[:k]
              
  
