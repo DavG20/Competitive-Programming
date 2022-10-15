8f38d65c912eb55e18b0fa95aae12aa47c5b7cfc
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2022-10-15 12:09:15 +0300

    Update Orginal_Array.py

--- LeetCode/Orginal_Array.py
+++ LeetCode/Orginal_Array.py
@@ -1,20 +1,14 @@
 class Solution(object):
-    def findOriginalArray(self,Changed):
-        freqNum={}
-        for i in range(len(Changed)):
-            if (Changed[i] in freqNum):
-                freqNum[Changed[i]]+=1
-            else:
-                freqNum[Changed[i]]=1
-        Changed.sort()
-        res=[]
-        for i in range(len(Changed)):
-            freq=freqNum[Changed[i]]
-            if freq>0 and freqNum[2*Changed[i]] :
-                res.append(Changed[i])
-                freqNum[Changed[i]]-=1
-                twice=2*Changed[i]
-                freqNum[twice]-=1
-            
-        return res
-
+    def findOriginalArray(self,changed):
+        count=Counter(changed)
+        result=[]
+        changed.sort()
+        for num in changed:
+            if count[num]==0:continue
+            if count[2*num]==0: return []
+            if num==0 and count[num]<=1:return []
+            result.append(num)
+            count[num]-=1
+            count[2*num]-=1
+        return result
+        
