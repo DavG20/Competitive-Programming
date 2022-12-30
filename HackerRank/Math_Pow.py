a8aef8046fef29005c87e5b1988b907c188bedef
DavG20 <dawit201121@gmail.com>
2022-12-30 21:47:04 +0300

    if else

---
+++ HackerRank/Math_Pow.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def Math_Pow(num1,num2):
+    print(num1**num2)
+def Math_Pow_Mod(num1,num2,num3):
+     print((num1**num2)%num3)
+
+    
+    
+if __name__=='__main__':
+    num1=int(input())
+    num2=int(input())
+    num3=int(input())
+    (Math_Pow(num1,num2))
+    (Math_Pow_Mod(num1,num2,num3))
