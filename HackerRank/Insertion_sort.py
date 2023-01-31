fb1150fb37e2347865313d35b3d55850293429cd
DavG20 <dawit201121@gmail.com>
2023-01-31 16:51:27 +0300

    insertion sort

--- HackerRank/Insertion_sort.py
+++ HackerRank/Insertion_sort.py
@@ -1,10 +1,17 @@
 def insertionSort1(n, arr):
     # Write your code here
-    target=arr[n-1]
-    index=n-2
-    while target<arr[index] and index>=0:
-        arr[index+1]=arr[index]
-        index-=1
-        print(*arr)
-    arr[index+1]=target
+    
+    
+    target = arr[n-1]
+    
+    index = n - 2
+    
+    while index >= 0 and  target < arr[index]:
+        
+        arr[index + 1] = arr[index ]
+        
+        index -= 1
+        print( *arr)
+    arr[index + 1] = target
+    
     print(*arr)
