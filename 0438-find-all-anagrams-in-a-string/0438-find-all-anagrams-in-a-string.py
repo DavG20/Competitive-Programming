b31a284747b64853b38e6ce32e0e7391475ffb37
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-02-22 17:39:22 +0300

    Time: 153 ms (55.94%), Space: 15.2 MB (74.27%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,55 +2,28 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         
         p_count = Counter(p)
-        res  = []
-        window = {}
-        have , need  = 0 , len(p_count)
-        l = 0 
-        for r in range(len(s)):
-            window[s[r]] = window.get(s[r], 0 ) + 1
+        
+        windows = {}
+        
+        arr = []
+        
+        left = 0
+        
+        for right in range(len(s)):
             
-            if window[s[r]] == p_count.get(s[r] , 0 ):
-                have += 1
-            elif window[s[r]] == p_count.get(s[r], 0 ) + 1:
-                have -= 1
+            windows[s[right]] = windows.get(s[right],0) + 1
             
-            
-            if r - l + 1 == len(p):
+            if right - left + 1 == len(p):
                 
-                if have == need:
-                    res.append(l)
+                if p_count == windows:
                     
+                    arr.append(left)
                 
+                windows[s[left]] -= 1
                 
-                if window[s[l]] == p_count.get(s[l], 0 ):
-                    have -= 1
+                if windows[s[left]] == 0:
+                    windows.pop(s[left])
+                left += 1
                 
-                elif window[s[l]] - 1 == p_count.get(s[l], 0 ):
-                    have += 1
+        return arr
                 
-                window[s[l]] -= 1
-                
-                l += 1
-        return res
-            
-            
-#         l = 0 
-#         p_counter = Counter(p)
-#         res = []
-#         window = {}
-#         for r in range(len(s)):
-            
-#             window[s[r]] = window.get(s[r], 0 ) + 1
-            
-#             if r - l + 1 == len(p):
-                
-#                 if window == p_counter:
-#                     res.append(l)
-                
-#                 window[s[l]] -= 1
-                
-#                 if window[s[l]] == 0:
-#                     window.pop(s[l])
-                    
-#                 l += 1
-#         return res
