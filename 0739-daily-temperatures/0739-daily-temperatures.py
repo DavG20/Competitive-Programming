70ed2d2e4df7a77750bbd745eee0d792e5fa3585
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-02-24 15:56:40 +0300

    Time: 1370 ms (87.12%), Space: 28.5 MB (77.22%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,25 +1,20 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         
+        
         stack = []
         
-        answer = [0 for i in range(len(temperatures))]
+        ans = [0] * len(temperatures)
+        
         
         for i in range(len(temperatures)):
             
-            while stack and stack[-1][0] < temperatures[i]:
+            while stack and temperatures[stack[-1]] < temperatures[i]:
                 
-                answer[stack[-1][1]] = i - stack[-1][1]
+                ans[stack[-1]] = i - stack[-1]
                 
                 stack.pop()
-            stack.append((temperatures[i],i))
+                
+            stack.append(i)
+        return ans
         
-        
-        return answer
-                
-                
-                
-                
-            
-            
-            
