01e68588e123674398af545c546f8d3ce4d667ec
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-03-03 11:08:01 +0300

    Time: 45 ms (47.19%), Space: 13.9 MB (46.90%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,34 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        start = 1
+        
+        end = x
+        
+        last = x
+        
+        while start <= end:
+            
+            mid = start + ( end - start) // 2
+            
+            if mid ** 2 > x :
+                
+                end = mid -1
+                
+            elif mid ** 2 < x :
+                last = mid
+                start = mid + 1
+                
+                
+            else:
+                return mid
+            
+        return last
+            
+            
+            
+            
+            
+        
+        
+        
