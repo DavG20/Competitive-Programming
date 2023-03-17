5e93cee0710b015ab8e596d19b39b383f5c3bc84
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-03-17 10:53:20 +0300

    Time: 588 ms (94.99%), Space: 27.8 MB (54.57%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -2,6 +2,9 @@ class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         
         
+        # start and end must instantiate by shifting to the right and to the left by one 
+        
+        
         start = 1
         
         end = len(arr) - 2
@@ -9,16 +12,20 @@ class Solution:
         
         while start <= end :
             
-            mid = start + (end - start) //2
+            mid = start + (end - start ) // 2
+            
+            # when our array is to the left of the mountain peak
             
             if arr[mid - 1] < arr[mid] < arr[mid + 1] :
                 
                 start = mid + 1
                 
-            elif arr[mid - 1] > arr[mid] > arr[mid + 1] :
+            # to the right of the peak 
+            elif  arr[mid - 1] > arr[mid] > arr[mid + 1] :
                 
                 end = mid - 1
                 
             else:
                 return mid
-                
+            
+        
