1a676d2f0d8312e417ce00a0a53ac572003664bf
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-03-25 22:27:08 +0300

    Update 0268-missing-number.py

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,43 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n=len(nums)
-        for i in range(n+1):
-            if i not in nums:
-                return i
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        
+        left = 0 
+        
+        missed_num = 0
+        
+        
+        while left < len(nums):
+            
+            if left < len(nums) and nums[left] == left :
+                
+                left += 1
+                
+            elif nums[left] == len(nums) :
+            
+                missed_num =  left
+                
+                
+                left += 1
+                
+            else:
+        
+                
+                nums[nums[left]] , nums[left] =  nums[left] , nums[nums[left]]
+                
+        if nums[missed_num] == missed_num:
+            
+            return len(nums)
+        
+        return missed_num
+                
+        
+#         left = 0 
+        
+#         distnict_num = set(nums)
+
+#         for i in range(len(nums) + 1):
+            
+#             if i not in distnict_num:
+#                 return i
+        
