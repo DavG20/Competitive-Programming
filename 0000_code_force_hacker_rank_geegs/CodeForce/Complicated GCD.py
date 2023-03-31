c2459864857132ab91d288dc26973234451bfac5
Dawit Teshager <dawit201121@gmail.com>
2023-03-31 22:35:01 +0300

    add GCD

--- 0000_code_force_hacker_rank_geegs/CodeForce/Complicated GCD.py
+++ 0000_code_force_hacker_rank_geegs/CodeForce/Complicated GCD.py
@@ -1,1 +1,14 @@
-gi
+a ,b = list(map(int , input().split()))
+
+
+def GCD(a , b):
+
+    if b == 0 :
+        return a
+    return GCD(b , a % b)
+
+if b - a >= 1:
+    print(1)
+else:
+    
+    print(GCD(a , b))
