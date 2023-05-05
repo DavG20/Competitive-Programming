7d343e56338c3814a4b1f57c0ac9a57ca7c6fc23
Dawit Teshager <dawit2011212gmail.com>
2023-05-05 23:52:56 +0300

    add heap sort

--- 0000_code_force_hacker_rank_geegs/Geeks/HeapSort.py
+++ 0000_code_force_hacker_rank_geegs/Geeks/HeapSort.py
@@ -1,33 +1,54 @@
+#User function Template for python3
+
 class Solution:
     #Heapify function to maintain heap property.
     def heapify(self,arr, n, i):
-        # code here
-        largest=i
-        l=2*i +1
-        r=2*i +2
-        if l<n and arr[i]<arr[l]:
-            largest=l
-        if r<n and arr[r]>arr[largest]:
-            largest=r
-        if largest!=i:
-            arr[i],arr[largest]=arr[largest],arr[i]
-            self.heapify(arr,n,largest)
+        # code here'
+        
+        smallchild = i 
+        
+        leftChild = 2 * i + 1
+        
+        rightChild = 2 * i + 2
+        
+        if leftChild < n and arr[leftChild] > arr[smallchild]:
+            smallchild = leftChild
+            
+        if rightChild < n and arr[rightChild] > arr[smallchild]:
+            smallchild = rightChild
+        
+        if smallchild != i :
+            
+            arr[i] , arr[smallchild] = arr[smallchild] , arr[i]
+            
+            self.heapify(arr ,n , smallchild )
+        
+        
+        
+        
+        
+        
     
     #Function to build a Heap from array.
     def buildHeap(self,arr,n):
         # code here
-        pass
         
+        for i in range(n//2 , -1 , -1):
+            
+            self.heapify(arr , n , i )
+            
+            
+            
+            
     
     #Function to sort an array using Heap Sort.    
     def HeapSort(self, arr, n):
         #code here
-        for i in range(n//2, -1, -1):
-          self.heapify(arr, n, i)
-          
-        for i in range(n-1, 0, -1):
-          # Swap
-          arr[i], arr[0] = arr[0], arr[i]
-  
-          # Heapify root element
-          self.heapify(arr, i, 0)
+        self.buildHeap(arr , n)
+        
+        for i in range(n - 1 , 0 , -1):
+            
+            arr[0] , arr[i] = arr[i] , arr[0]
+            
+            self.heapify(arr , i , 0)
+
