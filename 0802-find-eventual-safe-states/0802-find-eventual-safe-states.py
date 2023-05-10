4be4e6517360bec091e2a9a4a1bfca895774b67b
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-05-10 19:41:39 +0300

    Time: 685 ms (47.86%), Space: 24.3 MB (13.35%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,63 +1,61 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        visited = set()
+        
+        outDegree = defaultdict(int)
         
         Adj = defaultdict(list)
         
-        outdegree = defaultdict(int)
-        
-        ans = []
         for i in range(len(graph)):
             
-            outdegree[i] = len(graph[i])
+            outDegree[i] = len(graph[i])
             
             Adj[i] = graph[i]
             
-            
+        ans = []
         
         colors = [0] * len(graph)
-        print(outdegree , Adj ) 
+        
         for node in range(len(graph)):
             
-            cycle = self.hasCycle(node , colors , Adj , ans)
+            self.hasCycle(node , Adj , ans , colors)
             
-            
-            
-                        
-                    
-         
-        
         return sorted(ans)
-    
-    def hasCycle(self ,node , colors , adj , ans ):
+            
+    def hasCycle(self, node , Adj , ans , colors):
         
         if colors[node] == 2:
             
             return False
+        #has cycle because it goes to the currnet processing node
         if colors[node] == 1:
             
             return True
         
+        #change the node color to processing state
+        
         colors[node] = 1
         
-        for nbr in adj[node]:
+        for nbr in Adj[node]:
             
-            cycle = self.hasCycle(nbr, colors , adj , ans)
+            hasCycle = self.hasCycle(nbr , Adj , ans , colors)
             
-            if cycle :
-                return True
+            # becase the nbr is note a terminal or a safe node so before appending just return true
+            if hasCycle :
                 
-            
+                return True
         
         colors[node] = 2
         
         ans.append(node)
         
         return False
+            
         
+            
+            
+            
+            
+            
+            
+            
         
-            
-            
-            
-                
-     
