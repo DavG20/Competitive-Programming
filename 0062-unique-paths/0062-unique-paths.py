41fc3a5115e67360753db0c461422690c429b99b
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-05-31 18:20:53 +0300

    Time: 56 ms (6.41%), Space: 16.4 MB (21.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,8 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        @cache
-        def dp(point):
+        # @cache
+        memo = defaultdict(int)
+        def dp(point , memo):
             
             if point == (m - 1, n - 1):
                 return 1
@@ -11,12 +12,19 @@ class Solution:
             ways = 0
             
             if row + 1 < m :
-                
-                ways += dp((row + 1 , col))
+                if  (row + 1 , col) in memo:
+                    ways += memo[(row + 1 , col)]
+                else:
+                    memo[(row + 1 , col)] = dp((row + 1 , col) , memo)
+                    ways += memo[(row + 1 , col)]
             if col + 1 < n :
                 
-                ways += dp((row  , col + 1))
+                if  (row  , col + 1) in memo:
+                    ways += memo[(row  , col + 1)]
+                else:
+                    memo[(row  , col + 1)] = dp((row  , col + 1) , memo)
+                    ways += memo[(row  , col + 1)]
                 
             return ways 
                 
-        return dp((0 , 0))
+        return dp((0 , 0) , memo)
